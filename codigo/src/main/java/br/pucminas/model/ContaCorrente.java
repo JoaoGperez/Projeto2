699f5a3f5b1bec26f2d18db75e68bd64ee38e758
Marlene Moraes <marlenevmoraes@outlook.com>
2023-10-10 12:45:33 +0000

    code: modified currency account

--- codigo/src/main/java/br/pucminas/model/ContaCorrente.java
+++ codigo/src/main/java/br/pucminas/model/ContaCorrente.java
@@ -4,6 +4,8 @@ class ContaCorrente implements Conta {
     private double limiteSaqueEspecial = 200.0;
     private double taxaMensal = 20.0;
 
+    private List<Transacao> transacoes = new ArrayList<>();
+
     // Constructor
     public ContaCorrente() {
         this.saldo = 0.0;
@@ -17,14 +19,19 @@ class ContaCorrente implements Conta {
 
     @Override
     public void consultarExtrato() {
-        StringBuilder stringBuilder = new StringBuilder();
-        // add movimentacao da conta
-        System.out.println(stringBuilder);
+       StringBuilder extrato = new StringBuilder("Extrato da Conta Poupança:\n");
+        
+        for (Transacao transacao : transacoes) {
+            extrato.append(transacao).append("\n");
+        }
+
+        System.out.println(extrato);
     }
 
     @Override
     public String deposito(double valor) {
         saldo += valor;
+        registrarTransacao("D ", valor);
         return "Depósito efetuado com sucesso.";
     }
 
@@ -32,6 +39,7 @@ class ContaCorrente implements Conta {
     public String saque(double valor) {
         if (valor <= saldo + limiteSaqueEspecial) {
             saldo -= valor;
+            registrarTransacao("S ", -valor);
             return "Saque efetuado com sucesso.";
         } else {
             return "Não há saldo suficiente.";
@@ -42,10 +50,17 @@ class ContaCorrente implements Conta {
     public String transferencia(Conta destino, double valor) {
         if (sacar(valor)) {
             destino.depositar(valor);
+            registrarTransacao("T ", -valor);
             return "Transferência efetuada com sucesso.";
 
         } else {
             return "Não foi possível realizar esta transferência."; 
         }
     }
+
+    private void registrarTransacao(String descricao, double valor) {
+        Transacao transacao = new Transacao(LocalDate.now(), descricao, valor);
+        transacoes.add(transacao);
+    }
+}
 }
