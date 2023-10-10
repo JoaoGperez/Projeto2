b2ba2560110289be31deb3a452231de9658d5768
Marlene Moraes <marlenevmoraes@outlook.com>
2023-10-10 13:24:08 +0000

    code: refactoring to inheritance

--- codigo/src/main/java/br/pucminas/model/Conta.java
+++ codigo/src/main/java/br/pucminas/model/Conta.java
@@ -1,8 +1,67 @@
-interface Conta {
-    double consultarSaldo();
-    void consultarExtrato();
-    String deposito(double valor);
-    String saque(double valor);
-    String transferencia(Conta destino, double valor);
-    void registrarTransacao(String descricao, double valor);
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conta {
+    class ContaPoupanca implements Conta {
+        // Attributes
+        private double saldo;
+    
+        private List<Transacao> transacoes = new ArrayList<>();
+    
+        // Constructor
+        public Conta() {
+            this.saldo = 0.0;
+        }
+
+        public Conta(double saldo) {
+            this.saldo = saldo;
+        }
+    
+        // Methods
+        public double consultarSaldo() {
+            return saldo;
+        }
+    
+        public void consultarExtrato() {
+            StringBuilder extrato = new StringBuilder("Extrato da Conta Poupança:\n");
+            
+            for (Transacao transacao : transacoes) {
+                extrato.append(transacao).append("\n");
+            }
+    
+            extrato.toString();
+        }
+    
+        public String deposito(double valor) {
+            saldo += valor;
+            registrarTransacao("D ", valor);
+            return "Depósito efetuado com sucesso.";
+        }
+    
+        public String saque(double valor) {
+            if (valor <= saldo) {
+                saldo -= valor;
+                registrarTransacao("S ", -valor);
+                return "Saque efetuado com sucesso.";
+            } else {
+                return "Não há saldo suficiente.";
+            }
+        }
+    
+        public String transferencia(Conta destino, double valor) {
+            if (sacar(valor)) {
+                destino.depositar(valor);
+                registrarTransacao("T ", -valor);
+                return "Transferência efetuada com sucesso.";
+    
+            } else {
+                return "Não foi possível realizar esta transferência."; 
+            }
+        }
+    
+        private void registrarTransacao(String descricao, double valor) {
+            Transacao transacao = new Transacao(LocalDate.now(), descricao, valor);
+            transacoes.add(transacao);
+        }
+    }
 }
--- codigo/src/main/java/br/pucminas/model/ContaCorrente.java
+++ codigo/src/main/java/br/pucminas/model/ContaCorrente.java
@@ -1,42 +1,11 @@
-class ContaCorrente implements Conta {
+public class ContaCorrente extends Conta {
     // Attributes
-    private double saldo;
-    private double limiteSaqueEspecial = 200.0;
-    private double taxaMensal = 20.0;
-
-    private List<Transacao> transacoes = new ArrayList<>();
-
-    // Constructor
-    public ContaCorrente() {
-        this.saldo = 0.0;
-    }
+    private double limiteSaqueEspecial = 200.00;
+    private double taxaMensal = 20.00;
 
     // Methods
     @Override
-    public double consultarSaldo() {
-        return saldo;
-    }
-
-    @Override
-    public void consultarExtrato() {
-       StringBuilder extrato = new StringBuilder("Extrato da Conta Poupança:\n");
-        
-        for (Transacao transacao : transacoes) {
-            extrato.append(transacao).append("\n");
-        }
-
-        System.out.println(extrato);
-    }
-
-    @Override
-    public String deposito(double valor) {
-        saldo += valor;
-        registrarTransacao("D ", valor);
-        return "Depósito efetuado com sucesso.";
-    }
-
-    @Override
-    public String saque(double valor) {
+   public String saque(double valor) {
         if (valor <= saldo + limiteSaqueEspecial) {
             saldo -= valor;
             registrarTransacao("S ", -valor);
@@ -46,21 +15,11 @@ class ContaCorrente implements Conta {
         }
     }
 
-    @Override
-    public String transferencia(Conta destino, double valor) {
-        if (sacar(valor)) {
-            destino.depositar(valor);
-            registrarTransacao("T ", -valor);
-            return "Transferência efetuada com sucesso.";
-
-        } else {
-            return "Não foi possível realizar esta transferência."; 
+    private void calcularTaxaMensal() {
+        if (valor <= saldo + limiteSaqueEspecial) {
+            saldo -= taxaMensal;
+            registrarTransacao("TM ", taxaMensal);
         }
     }
-
-    private void registrarTransacao(String descricao, double valor) {
-        Transacao transacao = new Transacao(LocalDate.now(), descricao, valor);
-        transacoes.add(transacao);
-    }
-}
+ 
 }
