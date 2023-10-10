d43ab5345cce4d8cf1ecae290b3af95134b4c1c4
Marlene Moraes <marlenevmoraes@outlook.com>
2023-10-10 12:39:09 +0000

    code: create Transacoes class

---
+++ codigo/src/main/java/br/pucminas/model/Transacao.java
@@ -0,0 +1,16 @@
+public class Transacao {
+    private LocalDate data;
+    private String descricao;
+    private double valor;
+
+    public Transacao(LocalDate data, String descricao, double valor) {
+        this.data = data;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Data: " + data + ", " + descricao + ", Valor: " + valor;
+    }
+}
